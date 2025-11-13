5a84351eff1dfd34873b6e2f2cb76bb170374199
sebas2 <sr053990@gmail.com>
2025-11-12 21:14:59 -0500

    probando mi segund repositorio en github

---
+++ src/main/java/com/mycompany/sportparts/NewClass.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sportparts;
+
+/**
+ *
+ * @author PC
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("EL porno es bueno");
+    }
+        
+}
