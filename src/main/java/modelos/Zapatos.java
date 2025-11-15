82906e9bab1e776b88c16548007f24b8672ce0fe
sebas2 <sr053990@gmail.com>
2025-11-14 20:16:16 -0500

    Dando mas detalles a la capa de modelos

--- src/main/java/modelos/Zapatos.java
+++ src/main/java/modelos/Zapatos.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package modelos;
 
-/**
- *
- * @author PC
- */
+
 public class Zapatos extends Producto{
     private String marca;
     private String tipo;
